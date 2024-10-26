e04b8174a50b4f83b0cd4493fc5c9f2bdab3c87e
abhisheksh99 <abhishekleo1234@gmail.com>
2024-10-26 11:35:13 -0400

    Added custom font outfit

--- EV-CHARGING_STATION_APP/App.tsx
+++ EV-CHARGING_STATION_APP/App.tsx
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useCallback } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded] = useFonts({
+    'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold': require('./assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium': require('./assets/fonts/Outfit-SemiBold.ttf')
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (loaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  useEffect(() => {
+    onLayoutRootView();
+  }, [onLayoutRootView]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{
+        fontSize:20,
+        fontFamily:'outfit'
+      }}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
